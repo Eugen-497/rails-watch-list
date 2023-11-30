5ffe360cab5def4318f96667ecb3551b12f50aae
eugeniu velisca <veliscaeugeniu@gmail.com>
2023-11-30 17:18:38 +0100

    uuu

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,44 @@
+class ListsController < ApplicationController
+  def index
+    @lists = List.all.order(:created_at)
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to lists_path
+    else
+      render 'new'
+    end
+
+  end
+
+  def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    @list = List.find(params[:id])
+    if @list.update(list_params)
+      redirect_to lists_path
+    else
+      render 'edit'
+    end
+  end
+
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+
+  end
+end
